be0e4df5ef3493897c97d8bcf02c38cb4aaf6574
Greg W <gjwills@Gregs-MacBook-Pro.local>
2019-03-13 14:04:00 -0400

    testing Atom Github integration

--- parser.py
+++ parser.py
@@ -4,8 +4,14 @@ def tokenizer(cmd):
     # takes in sql command in string form
     # returns a list of tokens
     tokens = cmd.split(" ")
-    print(tokens)
+    eval = evaluator(tokens)
     return tokens
+
+def evaluator(token_list):
+    # takes in list of tokens in List form
+    # checks for SQL statement validity based on SQL grammar
+    
+    return True
 
 def main():
     cmd = ""
