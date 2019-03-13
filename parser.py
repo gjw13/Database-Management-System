ce06d98bf0fa9231eda60b050fee006221550d89
Greg W <gjwills@Gregs-MacBook-Pro.local>
2019-03-13 14:02:58 -0400

    added tokenizer

--- parser.py
+++ parser.py
@@ -1,8 +1,17 @@
 # testing command prompt
 
+def tokenizer(cmd):
+    # takes in sql command in string form
+    # returns a list of tokens
+    tokens = cmd.split(" ")
+    print(tokens)
+    return tokens
+
 def main():
     cmd = ""
     while cmd != "quit":
         cmd = raw_input("> ")
+        tokens = tokenizer(cmd)
+
 
 main()
