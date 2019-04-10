cf94e1ee998b65d1b995750ed2f0b43e27fb9193
Greg W <gjwills@Gregs-MacBook-Pro.local>
2019-04-10 17:06:19 -0400

    took out testing print statements

--- eval.py
+++ eval.py
@@ -22,9 +22,7 @@ def eval_select(cols, tables, conditions):
         if not conditions:
             simple_select(table,num_rows,num_cols,index_of_cols,testing)
         elif conditions:
-            print("length of conditions: " + str(len(conditions)))
             num_conditions = (len(conditions)+1)/2
-            print("number of conditions: " + str(num_conditions))
             if num_conditions == 1:
                 simple_where(table,columns,conditions,num_cols,num_rows,index_of_cols)
             else:
@@ -41,7 +39,6 @@ def eval_select(cols, tables, conditions):
             simple_select(table,num_rows,num_cols,index_of_cols,testing)
         elif conditions:
             # handles a complex select statement
-            col_index = 0
             num_conditions = (len(conditions)+1)/2
             if num_conditions == 1:
                 simple_where(table,columns,conditions,num_cols,num_rows,index_of_cols)
@@ -54,13 +51,12 @@ def complex_where(table,columns,conditions,num_cols,num_rows,index_of_cols,num_c
     matched_rows_list = []
     intersection_list = []
     union_list = []
-    # if conditions[1] == "and":
     for x in range(0,num_conditions):
         the_column = conditions[condition_num][0]
         the_value = conditions[condition_num][2]
         for item in columns:
             if the_column == item:
-                print(col_index)
+                # print(col_index)
                 break
             else:
                 col_index+=1
@@ -73,13 +69,13 @@ def complex_where(table,columns,conditions,num_cols,num_rows,index_of_cols,num_c
                 if list_of_vals[val][1] == the_value:
                     row_nums_matched.append(list_of_vals[val][0])
             matched_rows_list.append(row_nums_matched)
-            print(matched_rows_list)
+            # print(matched_rows_list)
             if len(matched_rows_list) > 1:
                 if conditions[1] == "and":
                     intersection_list = list(set(matched_rows_list[0]) & set(matched_rows_list[1]))
                 elif conditions[1] == "or":
                     intersection_list = list(set(matched_rows_list[0]) | set(matched_rows_list[1]))
-                print(intersection_list)
+                # print(intersection_list)
             result = []
             testing = index_of_cols[:]
             for g in intersection_list:
@@ -89,20 +85,19 @@ def complex_where(table,columns,conditions,num_cols,num_rows,index_of_cols,num_c
                 test = np.take(table,testing)
                 print_output(test)
         elif conditions[condition_num][1] == "!=":
-            print("in correct else if")
             for x in range(1,num_rows):
                 list_of_vals.append((x,np.take(table,x*num_cols+col_index+1)))
             for val in range(0,len(list_of_vals)):
                 if list_of_vals[val][1] != the_value:
                     row_nums_matched.append(list_of_vals[val][0])
             matched_rows_list.append(row_nums_matched)
-            print(matched_rows_list)
+            # print(matched_rows_list)
             if len(matched_rows_list) > 1:
                 if conditions[1] == "and":
                     intersection_list = list(set(matched_rows_list[0]) & set(matched_rows_list[1]))
                 elif conditions[1] == "or":
                     intersection_list = list(set(matched_rows_list[0]) | set(matched_rows_list[1]))
-                print(intersection_list)
+                # print(intersection_list)
             result = []
             testing = index_of_cols[:]
             for g in intersection_list:
@@ -119,7 +114,7 @@ def simple_where(table,columns,conditions,num_cols,num_rows,index_of_cols):
     first_val = conditions[0][2]
     for item in columns:
         if first_col == item:
-            print(col_index)
+            # print(col_index)
             break
         else:
             col_index+=1
@@ -135,7 +130,7 @@ def simple_where(table,columns,conditions,num_cols,num_rows,index_of_cols):
         if not row_nums_matched:
             print("The query did not return any results")
         else:
-            print(row_nums_matched)
+            # print(row_nums_matched)
             result = []
             testing = index_of_cols[:]
             for g in row_nums_matched:
@@ -153,7 +148,7 @@ def simple_where(table,columns,conditions,num_cols,num_rows,index_of_cols):
         if not row_nums_matched:
             print("The query did not return any results")
         else:
-            print(row_nums_matched)
+            # print(row_nums_matched)
             result = []
             # must select indices of cols for each row in row_nums_matched
             testing = index_of_cols[:]
@@ -213,7 +208,7 @@ def eval_create_table(table_name,cols):
     np.put(table,29,"jane")
     np.put(table,30,"doe")
     np.put(table,33,"greg")
-    print(table)
+    # print(table)
 
     return table, num_cols, m
 
@@ -223,7 +218,6 @@ def create_test_db(table,index,num_cols,row_num):
     np.put(table, row_num*num_cols+2,"smith")
     np.put(table, row_num*num_cols+3,"3700 O St. NW")
 
-
     return table
 
 def get_columns(table,num_cols):
@@ -232,7 +226,6 @@ def get_columns(table,num_cols):
         index_of_cols.append(x)
     columns = np.take(table,index_of_cols)
     return columns
-
 
 def eval_delete(table_name, conditions):
     # TODO:
